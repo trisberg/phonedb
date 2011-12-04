7fd7c6767c12351bc3e950454e1c7e12124bc48a
Niam Patel <naz1997@googlemail.com>
2011-12-04 19:39:21 +0000

    I just had to code wrestle

--- main.rb
+++ main.rb
@@ -1,7 +1,32 @@
 require 'digest/md5'
 require 'sinatra'
-require './data'
 require 'haml'
+require 'data_mapper'
+
+DataMapper.setup :default, "sqlite3://#{Dir.pwd}/database.db"
+
+class PhoneModels
+  include DataMapper::Resource
+
+  property :id          , Serial
+  property :make        , String
+  property :phone_model , String
+  property :price       , Float
+  property :created_at  , DateTime
+end
+
+class Customers
+  include DataMapper::Resource
+
+  property :id          , Serial
+  property :fname       , String
+  property :lname       , String
+  property :passwd      , String , :length => 18, :default => lambda { |pass| Digest::MD5.hexdigest(pass) }
+  property :created_at  , DateTime , :default => Time.now
+end
+
+PhoneModels.auto_upgrade!
+Customers.auto_upgrade!
 
 get '/' do
 	erb :index
@@ -23,18 +48,18 @@ get '/admin' do
   erb :admin
 end
 
-=begin
-post '/admin' do
-  modelEntry = Models.create { 
-    :make       => params[:make], 
-    :model      => params[:model], 
-    :price      => params[:price], 
-    :desc       =>  params[:desc], 
-    :created_at => Time.now }
 
-  customerEntry = Customers.new {
-    fname: params[:fname],
-    lname: params[:lname],
-    passwd: params[:passwd]
-  }
-end=end
+# post '/admin' do
+#   modelEntry = Models.create { 
+#     :make       => params[:make], 
+#     :phone_model      => params[:model], 
+#     :price      => params[:price], 
+#     :desc       =>  params[:desc], 
+#     :created_at => Time.now }
+
+#   customerEntry = Customers.new {
+#     fname: params[:fname],
+#     lname: params[:lname],
+#     passwd: params[:passwd]
+#   }
+# end
